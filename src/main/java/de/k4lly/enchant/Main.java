4f30531072228883e52c856762b0b579495e1bf9
BuildTools <unconfigured@null.spigotmc.org>
2016-03-03 22:00:10 +0100

    Hallo Welt

--- src/main/java/de/k4lly/enchant/Main.java
+++ src/main/java/de/k4lly/enchant/Main.java
@@ -4,10 +4,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
     public void onEnable() {
-
+        System.out.println("Hallo Welt");
     }
 
     public void onDisable() {
-        
+
     }
 }
