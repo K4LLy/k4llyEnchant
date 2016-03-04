3820f0f4eb2d1ed624c4f2b142244853042a1a8e
K4LLy <k4llylp@gmail.com>
2016-03-04 22:52:46 +0100

    Changed Commands and Configs

--- src/main/java/de/k4lly/enchant/Main.java
+++ src/main/java/de/k4lly/enchant/Main.java
@@ -3,10 +3,12 @@ package de.k4lly.enchant;
 import de.k4lly.enchant.controller.ConfigController;
 import de.k4lly.enchant.controller.PluginController;
 import de.k4lly.enchant.listener.Command;
+import de.k4lly.enchant.listener.Join;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin{
 
     private PluginController controller = new PluginController();
     private String enable = "[k4llyEnchant] Loaded " + this.getDescription().getName() + " in Version " + this.getDescription().getVersion() + " succesfully!";
@@ -24,6 +26,7 @@ public class Main extends JavaPlugin {
         Command command = new Command(controller);
         this.getCommand("k4llyEnchant").setExecutor(command);
         this.getCommand("kE").setExecutor(command);
+        Join join = new Join(controller);
     }
 
     @Override
---
+++ src/main/java/de/k4lly/enchant/listener/Join.java
@@ -0,0 +1,21 @@
+package de.k4lly.enchant.listener;
+
+import de.k4lly.enchant.controller.PluginController;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class Join implements Listener {
+
+    private PluginController controller;
+
+    public Join(PluginController controller) { this.controller=controller; }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent playerJoinEvent) {
+        Player player = playerJoinEvent.getPlayer();
+        player.sendMessage(ChatColor.AQUA + "Thanks for using K4LLyEnchant v." + controller.getMain().getDescription().getVersion());
+    }
+}
